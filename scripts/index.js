ff8a1a50cee6aec05c53baf94f1de0724725b377
Fernando <costasfernando199@gmail.com>
2021-08-06 23:00:12 -0300

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+
+const modal = document.getElementById('myModal');
+const modalImg = document.getElementById('img01');
+const captionText = document.getElementById('caption');
+const span = document.getElementsByClassName('close')[0];
+
+span.onclick=function(){
+    modal.style.display="none"
+}
+
+function clique(img) {
+    modal.style.display="block";
+    modalImg.src=img.src;
+    modalImg.alt=img.alt;
+	captionText.innerHTML=img.alt;
+
+}
